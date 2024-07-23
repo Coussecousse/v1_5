e1cb2a39096b02d0c4f9c7f2c434d6d834d1cb50
Erika <adob.e@hotmail.com>
2024-07-24 00:40:19 +0200

    feat: start the homepage style

--- assets/js/App.js
+++ assets/js/App.js
@@ -2,11 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../styles/app.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout'
 import Home from './components/Home/Home';
-
-
+import paths from './config/paths';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -14,9 +13,11 @@ root.render(
   // <React.StrictMode>
     <BrowserRouter>
         <div className="App">
-            <Layout>
-                <Home />
-            </Layout>
+          <Layout>
+            <Routes>
+              <Route path={paths.HOME} element={<Home></Home>}></Route>
+            </Routes>
+          </Layout>
         </div>
     </BrowserRouter>
   // </React.StrictMode> 
--- assets/js/components/Home/Home.js
+++ assets/js/components/Home/Home.js
@@ -1,29 +1,19 @@
-// ./assets/js/components/Home.js
-    
 import React, {Component} from 'react';
-import {Route, Switch,Redirect, Link, withRouter} from 'react-router-dom';
 import styles from './Home.module.css';
     
 class Home extends Component {
     
     render() {
         return (
-           <div className={styles.test}>
-               <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                   <Link className={"navbar-brand"} to={"/"}> Symfony React Project </Link>
-                   <div className="collapse navbar-collapse" id="navbarText">
-                       <ul className="navbar-nav mr-auto">
-                           <li className="nav-item">
-                               <Link className={"nav-link"} to={"/posts"}> Posts </Link>
-                           </li>
-    
-                           <li className="nav-item">
-                               <Link className={"nav-link"} to={"/users"}> Users </Link>
-                           </li>
-                       </ul>
-                   </div>
-               </nav>
-           </div>
+           <section className={styles.main}>
+                <div className={styles.backgroundImage}></div>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Préparez votre voyage<br/><span className={styles.secondTitle}>simplement</span><span className={styles.andTitle}> et </span><span className={styles.secondTitle}>rapidement</span></h1>
+                    <div className={styles.scroll}>
+                        <span>scroll</span>
+                    </div>
+                </div>
+           </section>
         )
     }
 }
