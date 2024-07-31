ace08b99f8c63d209b0b0f355a3e0922be14f6c6
Erika <adob.e@hotmail.com>
2024-07-31 14:31:52 +0200

    feat; adding compass

--- assets/js/components/Home/Home.js
+++ assets/js/components/Home/Home.js
@@ -10,6 +10,7 @@ class Home extends Component {
                 <div className={styles.content}>
                     <h1 className={styles.title}>Préparez votre voyage<br/><span className={styles.secondTitle}>simplement</span><span className={styles.andTitle}> et </span><span className={styles.secondTitle}>rapidement</span></h1>
                     <div className={styles.scroll}>
+                        <div className={styles.compass}></div>
                         <span>scroll</span>
                     </div>
                 </div>
