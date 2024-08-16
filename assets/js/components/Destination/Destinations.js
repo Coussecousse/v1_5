93f7f03a985f1e52ec3f6037e17ef26c53cba632
Erika <adob.e@hotmail.com>
2024-08-16 17:53:33 +0200

    feat: adding second part

--- assets/js/components/Destination/Destinations.js
+++ assets/js/components/Destination/Destinations.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Main from "./Main/Main";
+import Second from "./Second/Second";
 
 
 export default function Destinations() {
     return (
         <>
             <Main />
+            <Second />
         </>
     )
 }
