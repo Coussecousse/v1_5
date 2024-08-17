757e397207439196f2e6f817af0637c64d6b013d
Erika <adob.e@hotmail.com>
2024-08-17 16:28:34 +0200

    feat: adding third part , join and footer

--- assets/js/components/Community/Community.js
+++ assets/js/components/Community/Community.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Main from "./Main/Main";
 import Second from "./Second/Second";
+import Third from "./Third/Third";
+import Join from "../../containers/Join/Join";
+import Footer from "../../containers/Footer/Footer";
 
 export default function Community() {
     return (
         <>
             <Main />
             <Second />
+            <Third />
+            <Join />
+            <Footer />
         </>
     )
 }
