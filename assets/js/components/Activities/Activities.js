acdb1ea14e4a49126e21194a2a1871201f554ef0
Erika <adob.e@hotmail.com>
2024-10-09 16:15:16 +0200

    feat: add form style for search

--- assets/js/components/Activities/Activities.js
+++ assets/js/components/Activities/Activities.js
@@ -8,87 +8,39 @@ import { Link } from "react-router-dom";
 import paths from "../../config/paths";
 
 export default function Activities() {
-    // const [flashMessage, setFlashMessage] = useState(null);
     const [loading, setLoading] = useState(false);
-    // const [errors, setErrors] = useState({});
-    // const [query, setQuery] = useState('');
-    // const [suggestions, setSuggestions] = useState([]);
-    // const debounceTimeout = useRef(null);
-    // const [locationQuery, setLocationQuery] = useState('');
-    // const [JSONLocation, setJSONLocation] = useState({});
-    // const [selectionnedLocation, setSelectionnedLocation] = useState(null);
+    const [query, setQuery] = useState('');
+    const [suggestions, setSuggestions] = useState([]);
+    const debounceTimeout = useRef(null);
+    const [errors, setErrors] = useState({});
 
-    // const navigate = useNavigate();
+    const handleTypeChange = (event) => {
+        const value = event.target.value;
+        setQuery(value);
 
-    // const handleInputChange = (event) => {
-    //     const value = event.target.value;
-    //     setQuery(value);
+        if (debounceTimeout.current) {
+            clearTimeout(debounceTimeout.current);
+        }
 
-    //     if (debounceTimeout.current) {
-    //         clearTimeout(debounceTimeout.current);
-    //     }
+        debounceTimeout.current = setTimeout(async () => {
+            if (value.length > 1) {
+                try {
+                    const response = await axios.get(`/api/tags/autocomplete?q=${value}`);
+                    setSuggestions(response.data);
+                } catch (error) {
+                    console.error('Error fetching autocomplete suggestions', error);
+                }
+            } else {
+                setSuggestions([]);
+            }
+        }, 300); 
+    };
 
-    //     debounceTimeout.current = setTimeout(async () => {
-    //         if (value.length > 1) {
-    //             try {
-    //                 const response = await axios.get(`/api/tags/autocomplete?q=${value}`);
-    //                 setSuggestions(response.data);
-    //             } catch (error) {
-    //                 console.error('Error fetching autocomplete suggestions', error);
-    //             }
-    //         } else {
-    //             setSuggestions([]);
-    //         }
-    //     }, 300); 
-    // };
-
-    // const handleSuggestionClick = (suggestion) => {
-    //     setQuery(suggestion.name);
-    //     setSuggestions([]); 
-    // };
-
-    // useEffect(() => {
-    //     if (debounceTimeout.current) {
-    //         clearTimeout(debounceTimeout.current);
-    //     }
-    // }, []);
-
-    // const handleSearchLocation = (event) => {
-    //     const value = event.target.value;
-    //     setLocationQuery(value);
-    // };
-
-    // useEffect(() => {
-    //     if (debounceTimeout.current) {
-    //         clearTimeout(debounceTimeout.current);
-    //     }
-
-        
-    //     debounceTimeout.current = setTimeout(async () => {
-    //         if (locationQuery.length > 1) {
-    //             try {
-    //                 const response = await axios.get(`https://us1.locationiq.com/v1/search?key=${config.key}&q=${locationQuery}&format=json&`);
-    //                 setJSONLocation(response.data);
-    //             } catch (error) {
-    //                 console.error('Error fetching autocomplete suggestions', error);
-    //             }
-    //         } else {
-    //             setSuggestions([]);
-    //         }
-    //     }, 150); 
-    // }, [locationQuery]);
-
-    // const handleSubmit = e => {
-    //     e.preventDefault();
-
-    //     if (!selectionnedLocation) {
-    //         setFlashMessage({ type: 'error', message: 'Please select a location on the map.' });
-    //         return;
-    //     }
-
-    //     navigate('/activities/create', { state: { address: selectionnedLocation } }); 
-    // }
-
+    const handleSuggestionClick = (suggestion) => {
+        setQuery(suggestion.name);
+        setSuggestions([]); 
+    };
+    
     return (
         <section className={`first-section ${styles.section}`}>
             <h1 className={`typical-title ${styles.title}`}>Activités</h1>
@@ -99,7 +51,48 @@ export default function Activities() {
                         <span className={`loader-text ${profileStyles.loaderText}`}>Chargement...</span>
                     </div>
                 ) : (
-                    <Link to={paths.CREATE_ACTIVITY}>Créer une activité</Link>
+                    <div className={`${styles.actvitiesContainer}`}>
+                        <Link to={paths.CREATE_ACTIVITY}>Créer une activité</Link>
+                        <h2 className={styles.secondTitle}>Chercher une activité dans la communauté</h2>
+                        <form className={styles.searchFormContainer}>
+                            <div className={styles.formFieldsContainer}>
+                                <div className={styles.searchInputContainer}>
+                                    <label>Localisation :</label>
+                                    <input type="text" placeholder="Localisation" id="localisation" name="localisation"></input>
+                                </div>
+                                <div className={styles.autocompleteContainer}>
+                                    <label htmlFor="map_type">Type* :</label>
+                                    <div className={``}>
+                                        <input
+                                            type="text"
+                                            value={query}
+                                            onChange={handleTypeChange}
+                                            placeholder="Type"
+                                            id="type"
+                                            name="type"
+                                        />
+                                        {suggestions.length > 0 && (
+                                            <ul className={formStyles.suggestionsList}>
+                                                {suggestions.map((suggestion) => (
+                                                    <li key={suggestion.id} onClick={() => handleSuggestionClick(suggestion)}>
+                                                        {suggestion.name}
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        )}
+                                    </div>
+                                </div>
+                                <input className={`${styles.submit}`} type="submit" value="Chercher" />
+                            </div>
+                            {errors.length > 0 && (
+                                <div className={formStyles.errorContainer}>
+                                    {errors.map((error, index) => (
+                                        <small key={index} className={formStyles.error}>{error}</small>
+                                    ))}
+                                </div>
+                            )}
+                        </form>
+                    </div>
                     )
                 }
             </div>
