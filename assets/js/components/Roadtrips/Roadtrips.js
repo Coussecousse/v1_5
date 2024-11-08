72d13c682874e92fd8a359970edfa3d8eecc70e8
Erika <adob.e@hotmail.com>
2024-11-08 11:34:14 +0100

    feat: style search form for roadtrips

--- assets/js/components/Roadtrips/Roadtrips.js
+++ assets/js/components/Roadtrips/Roadtrips.js
@@ -65,109 +65,113 @@ export default function Roadtrips() {
             <h1 className={`typical-title ${styles.title}`}>Roadtrips</h1>
             <div className={styles.container}>
                 <div className={`${styles.roadtripsContainer}`}>
-                    <h2>Chercher un roadtrip dans la communauté</h2>
-                    <div>
+                    <h2 className={styles.secondTitle}>Chercher un roadtrip dans la communauté</h2>
+                    <div className={styles.linkContainer}>
                         <p>Ou vous préférez peut être...</p>
                         <Link to={paths.CREATE_ROADTRIP} className={`${styles.createLink} link`}>Créer un roadtrip</Link>
                     </div>
-                </div>
-                <form className={styles.searchRoadtrip} ref={formRef}>
-                    <div className={styles.formPrincipal}>
-                        <div className={styles.countyInputContainer}>
-                            <label htmlFor="country">Pays disponibles</label>
-                            <div>
-                                <input
-                                    type="text"
-                                    value={query}
-                                    onChange={handleCountryChange}
-                                    id="country"
-                                    name="country">
-                                </input>
-                                {countries.length > 0 && (
-                                    <ul className={formStyles.countriesList}>
-                                        {countries.map((country, index) => (
-                                            <li key={index} onClick={() => handleCountrySelection(country)}>{country}</li>
-                                        ))}
-                                    </ul>
-                                )}
+                    <form className={styles.searchRoadtrip} ref={formRef}>
+                        <div className={styles.formPrincipal}>
+                            <div className={styles.countryInputContainer}>
+                                <label htmlFor="country">Pays disponibles :</label>
+                                <div>
+                                    <input
+                                        type="text"
+                                        value={query}
+                                        onChange={handleCountryChange}
+                                        id="country"
+                                        name="country">
+                                    </input>
+                                    {countries.length > 0 && (
+                                        <ul className={formStyles.countriesList}>
+                                            {countries.map((country, index) => (
+                                                <li key={index} onClick={() => handleCountrySelection(country)}>{country}</li>
+                                            ))}
+                                        </ul>
+                                    )}
+                                </div>
                             </div>
+                            <div className={styles.filter}>
+                                <label htmlFor="filter">Filtres :</label>
+                                <select name="filter" id="filter">
+                                    <option value="all">Tous</option>
+                                    <option value="mostRecent">Les plus récents</option>
+                                    <option value="mostPopular">Les plus populaires</option>
+                                </select>
+                            </div>
+                            {errors.form && (
+                                <div className={formStyles.errorsContainer}>
+                                    {Object.keys(errors).map((key) => (
+                                        <small key={key} className={`${formStyles.errorGreen} ${styles.error}`}>
+                                            <div className={styles.errorIcon}></div>{errors[key]}
+                                        </small>
+                                    ))}
+                                </div>
+                            )}
+                            <input className={`${styles.submit}`} type="submit" value="Chercher" />
                         </div>
-                        <div className={styles.filter}>
-                            <label htmlFor="filter">Filtres</label>
-                            <select name="filter" id="filter">
-                                <option value="all">Tous</option>
-                                <option value="mostRecent">Les plus récents</option>
-                                <option value="mostPopular">Les plus populaires</option>
-                            </select>
+                        <div className={styles.options}>
+                            <fieldset>
+                                <legend className={styles.legend}>Prix :</legend>
+                                <ul className={styles.optionsList}>
+                                    <li>
+                                        <input type="radio" id="price_1" name="price" value="price_1" />
+                                        <label>€</label>
+                                    </li>
+                                    <li>
+                                        <input type="radio" id="price_2" name="price" value="price_2" />
+                                        <label>€ €</label>
+                                    </li>
+                                    <li>
+                                        <input type="radio" id="price_3" name="price" value="price_3" />
+                                        <label>€ € €</label>
+                                    </li>
+                                </ul>
+                            </fieldset>
+                            <fieldset>
+                                <legend className={styles.legend}>Durée :</legend>
+                                <ul className={styles.optionsList}>
+                                    <li>
+                                        <input type="radio" id="duration_1" name="duration" value="duration_1" />
+                                        <label>1 à 7 jours</label>
+                                    </li>
+                                    <li>
+                                        <input type="radio" id="duration_2" name="duration" value="duration_2" />
+                                        <label>8 à 15 jours</label>
+                                    </li>
+                                    <li>
+                                        <input type="radio" id="duration_3" name="duration" value="duration_3" />
+                                        <label>15 jours et plus</label>
+                                    </li>
+                                </ul>
+                            </fieldset>
                         </div>
-                        {errors.form && (
-                            <div className={formStyles.errorsContainer}>
-                                {Object.keys(errors).map((key) => (
-                                    <small key={key} className={`${formStyles.errorGreen} ${styles.error}`}>
-                                        <div className={styles.errorIcon}></div>{errors[key]}
-                                    </small>
-                                ))}
-                            </div>
-                        )}
-                        <input className={`${styles.submit}`} type="submit" value="Chercher" />
+                    </form>
+                    {loading ? (
+                        <div className={`${styles.loaderContainer} loader-container`}>
+                            <span className={`loader ${formStyles.loader} ${formStyles.loaderGreen}`}></span>
+                            <span className={`loader-text ${formStyles.loaderTextGreen}`}>Chargement...</span>
+                        </div>
+                    ) : (
+                        <div className={styles.roadtrips}>
+                            {currentRoadtrips.length > 0 ? (
+                                currentRoadtrips.map((roadtrip, index) => (
+                                    <div key={index}>Roadtrip</div>
+                                ))
+                            ) : (
+                                <p>Aucun roadtrip trouvé</p>
+                            )}
+                        </div>
+                    )}
+                    <div className={styles.pagination}>
+                        <button onClick={handlePrevPage} disabled={currentPage === 1} aria-label="Precedent">❮</button>
+                        {Array.from({ length: totalPages }, (_, index) => (
+                            <button key={index + 1} onClick={() => handlePageClick(index + 1)} className={currentPage === index + 1 ? styles.active : ''}>
+                                {index + 1}
+                            </button>
+                        ))}
+                        <button onClick={handleNextPage} disabled={currentPage === totalPages} aria-label="Suivant">❯</button>
                     </div>
-                    <div className={styles.options}>
-                        <fieldset>
-                            <legend>Prix :</legend>
-                            <div>
-                                <input type="radio" id="price_1" name="price" value="price_1" />
-                                <label>€</label>
-                            </div>
-                            <div>
-                                <input type="radio" id="price_2" name="price" value="price_2" />
-                                <label>€€</label>
-                            </div>
-                            <div>
-                                <input type="radio" id="price_3" name="price" value="price_3" />
-                                <label>€€€</label>
-                            </div>
-                        </fieldset>
-                        <fieldset>
-                            <legend>Durée :</legend>
-                            <div>
-                                <input type="radio" id="duration_1" name="duration" value="duration_1" />
-                                <label>1 à 7 jours</label>
-                            </div>
-                            <div>
-                                <input type="radio" id="duration_2" name="duration" value="duration_2" />
-                                <label>8 à 15 jours</label>
-                            </div>
-                            <div>
-                                <input type="radio" id="duration_3" name="duration" value="duration_3" />
-                                <label>15 jours et plus</label>
-                            </div>
-                        </fieldset>
-                    </div>
-                </form>
-                {loading ? (
-                    <div className={`${styles.loaderContainer} loader-container`}>
-                        <span className={`loader ${formStyles.loader} ${formStyles.loaderGreen}`}></span>
-                        <span className={`loader-text ${formStyles.loaderTextGreen}`}>Chargement...</span>
-                    </div>
-                ) : (
-                    <div className={styles.roadtrips}>
-                        {currentRoadtrips.length > 0 ? (
-                            currentRoadtrips.map((roadtrip, index) => (
-                                <div key={index}>Roadtrip</div>
-                            ))
-                        ) : (
-                            <p>Aucun roadtrip trouvé</p>
-                        )}
-                    </div>
-                )}
-                <div className={styles.pagination}>
-                    <button onClick={handlePrevPage} disabled={currentPage === 1} aria-label="Precedent">❮</button>
-                    {Array.from({ length: totalPages }, (_, index) => (
-                        <button key={index + 1} onClick={() => handlePageClick(index + 1)} className={currentPage === index + 1 ? styles.active : ''}>
-                            {index + 1}
-                        </button>
-                    ))}
-                    <button onClick={handleNextPage} disabled={currentPage === totalPages} aria-label="Suivant">❯</button>
                 </div>
             </div>
         </section>
