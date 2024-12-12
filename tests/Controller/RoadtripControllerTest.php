fdaec489320bce412096d9853314b5d9f4622796
Erika <adob.e@hotmail.com>
2024-12-12 16:38:06 +0100

    test: add more test for creating roadtrip

--- tests/Controller/RoadtripControllerTest.php
+++ tests/Controller/RoadtripControllerTest.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RoadtripControllerTest extends WebTestCase
 {
+    // Test with good data
     public function testCreateRoadtrip(): void
     {
         $client = static::createClient();
@@ -68,4 +69,178 @@ class RoadtripControllerTest extends WebTestCase
 
         $this->assertSame('Roadtrip created', $responseData['status']);
     }
+
+
+    // Test without being logged in
+    public function testCreateRoadtripWithoutLogin(): void
+    {
+        $client = static::createClient();
+
+        $data = [
+            'title' => 'Test Roadtrip',
+            'country' => 'France',
+            'days' => json_encode([
+                [
+                    [
+                        "informations" => [
+                            "display_name" => "Paris, Île-de-France, France métropolitaine, France",
+                            "lat" => "48.8588897",
+                            "lng" => "2.3200410217200766"
+                        ]
+                    ]
+                ],
+                [
+                    [
+                        "informations" => [
+                            "display_name" => "Bordeaux, Gironde, Nouvelle-Aquitaine, France métropolitaine, France",
+                            "lat" => "44.841225",
+                            "lng" => "-0.5800364"
+                        ]
+                    ],
+                    [
+                        "informations" => [
+                            "display_name" => "Mimizan, Mont-de-Marsan, Landes, Nouvelle-Aquitaine, France métropolitaine, 40200, France",
+                            "lat" => "44.2019796",
+                            "lng" => "-1.2309278"
+                        ]
+                    ]
+                ]
+            ]),
+            'roads' => json_encode([
+                [],
+                [
+                    ['route' => 'supposed to be between Paris and Bordeaux'],
+                    ['route' => 'supposed to be between Bordeaux and Mont de Marsan']
+                ]
+            ])
+        ];
+
+        $client->request('POST', '/api/roadtrip/create', $data);
+
+        $this->assertResponseStatusCodeSame(500); 
+    }
+
+    // Test with empty data
+    public function testCreateRoadtripWithMissingTitle(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneBy(['email' => 'test@hotmail.com']);
+        $client->loginUser($user);
+
+        $data = [
+            'country' => 'France',
+            'days' => json_encode([
+                [
+                    [
+                        "informations" => [
+                            "display_name" => "Paris, Île-de-France, France métropolitaine, France",
+                            "lat" => "48.8588897",
+                            "lng" => "2.3200410217200766"
+                        ]
+                    ]
+                ],
+                [
+                    [
+                        "informations" => [
+                            "display_name" => "Bordeaux, Gironde, Nouvelle-Aquitaine, France métropolitaine, France",
+                            "lat" => "44.841225",
+                            "lng" => "-0.5800364"
+                        ]
+                    ],
+                    [
+                        "informations" => [
+                            "display_name" => "Mimizan, Mont-de-Marsan, Landes, Nouvelle-Aquitaine, France métropolitaine, 40200, France",
+                            "lat" => "44.2019796",
+                            "lng" => "-1.2309278"
+                        ]
+                    ]
+                ]
+            ]),
+            'roads' => json_encode([
+                [],
+                [
+                    ['route' => 'supposed to be between Paris and Bordeaux'],
+                    ['route' => 'supposed to be between Bordeaux and Mont de Marsan']
+                ]
+            ])
+        ];
+
+        $client->request('POST', '/api/roadtrip/create', $data);
+
+        $this->assertResponseStatusCodeSame(400); 
+        $this->assertStringContainsString('title', $client->getResponse()->getContent());
+    }
+
+    public function testCreateRoadtripWithMissingCountry(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneBy(['email' => 'test@hotmail.com']);
+        $client->loginUser($user);
+
+        $data = [
+            'title' => 'France',
+            'days' => json_encode([
+                [
+                    [
+                        "informations" => [
+                            "display_name" => "Paris, Île-de-France, France métropolitaine, France",
+                            "lat" => "48.8588897",
+                            "lng" => "2.3200410217200766"
+                        ]
+                    ]
+                ],
+                [
+                    [
+                        "informations" => [
+                            "display_name" => "Bordeaux, Gironde, Nouvelle-Aquitaine, France métropolitaine, France",
+                            "lat" => "44.841225",
+                            "lng" => "-0.5800364"
+                        ]
+                    ],
+                    [
+                        "informations" => [
+                            "display_name" => "Mimizan, Mont-de-Marsan, Landes, Nouvelle-Aquitaine, France métropolitaine, 40200, France",
+                            "lat" => "44.2019796",
+                            "lng" => "-1.2309278"
+                        ]
+                    ]
+                ]
+            ]),
+            'roads' => json_encode([
+                [],
+                [
+                    ['route' => 'supposed to be between Paris and Bordeaux'],
+                    ['route' => 'supposed to be between Bordeaux and Mont de Marsan']
+                ]
+            ])
+        ];
+
+        $client->request('POST', '/api/roadtrip/create', $data);
+
+        $this->assertResponseStatusCodeSame(400); 
+        $this->assertStringContainsString('country', $client->getResponse()->getContent());
+    }
+
+    public function testCreateRoadtripWithEmptyDaysAndRoads(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneBy(['email' => 'test@hotmail.com']);
+        $client->loginUser($user);
+
+        $data = [
+            'title' => 'Test Roadtrip',
+            'country' => 'France',
+            'days' => '[]',
+            'roads' => '[]'
+        ];
+
+        $client->request('POST', '/api/roadtrip/create', $data);
+
+        $this->assertResponseStatusCodeSame(400);
+        $this->assertStringContainsString('roadtrip', $client->getResponse()->getContent());
+    }
+
 }
